281a5032c856cfc33c77c6adae1d868aa161ecb0
Javier Casal <javier.casal@mulesoft.com>
2014-06-25 17:00:46 -0300

    using DateUtils methods for comparison (SIT-415 completed)

--- src/test/java/org/mule/templates/integration/BusinessLogicTest.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicTest.java
@@ -20,7 +20,6 @@ import java.util.Properties;
 import junit.framework.Assert;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.junit.After;
